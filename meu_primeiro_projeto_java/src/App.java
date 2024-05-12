23cf3261996c922a2299c06f9706176b51335dc9
wendellCR73 <wwendell30@gmail.com>
2024-05-12 18:15:03 -0300

    Meu_primeiro_projetinho_java

---
+++ meu_primeiro_projeto_java/src/App.java
@@ -0,0 +1,5 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Sejam Bem-Vindos ao meu primeiro projeto JAVA (ou uma tentativa dele)!");
+    }
+}
